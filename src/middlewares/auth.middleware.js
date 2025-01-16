03025a442632baa8e2e4ebb8b3465585753e9188
vansh-000 <23165@iiitu.ac.in>
2025-01-17 00:37:26 +0530

    standardising errors

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 import { User } from '../models/user.model.js';
+import { ApiError } from '../utils/ApiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 // if res is not used we replace it by _ to avoid warning
@@ -10,17 +11,22 @@ export const validateJWT = asyncHandler(async (req, _, next) => {
         if (!token) {
             return res.status(401).json({ message: 'Unauthorized Token' });
         }
-        const decoded = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const decoded = await jwt.verify(
+            token,
+            process.env.ACCESS_TOKEN_SECRET
+        );
         // access token has a field called _id
         const user = await User.findById(decoded?._id).select(
             '-password -refreshToken'
         );
         if (!user) {
-            return res.status(401).json({ message: 'Invalid Access Token' });
+            return res
+                .status(401)
+                .json(new ApiError('Invalid access token', 401));
         }
         req.user = user;
         next();
     } catch (error) {
-        return res.status(401).json({ message: 'Invalid Access Token' });
+        return res.status(401).json(new ApiError('Invalid access token', 401));
     }
 });
